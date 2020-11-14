3ea4f6782ad55c08781d9d005c190d014d5a45a1
Ben Laplanche <ben@laplanche.co.uk>
2020-11-14 15:39:06 +0000

    latest build

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 )
 
@@ -10,15 +9,12 @@ func main() {
 	// fmt.Println("SUCCESS")
 	filename := "terraform.tf"
 	args := []string{"test", string(filename), "--policy=packs/terraform"}
-	out, err := exec.Command("conftest", args...).Output()
-	// fmt.Printf("Test results are...", out)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
+	out, _ := exec.Command("conftest", args...).Output()
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, err)
+	// }
 
 	if out != nil {
-		// fmt.Fprintln(os.Stdout, out)
-		// fmt.Printf("Test results are...\n %v", out)
 		fmt.Print(string(out))
 	}
 }
