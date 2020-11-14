363f503574cf4aa693812bdf2409cdc5915d7b06
Ben Laplanche <ben@laplanche.co.uk>
2020-11-14 15:50:10 +0000

    switched to using cobra

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"fmt"
-	"os/exec"
-)
+import "snyky/cmd"
 
 func main() {
-	// fmt.Println("SUCCESS")
-	filename := "terraform.tf"
-	args := []string{"test", string(filename), "--policy=packs/terraform"}
-	out, _ := exec.Command("conftest", args...).Output()
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, err)
-	// }
-
-	if out != nil {
-		fmt.Print(string(out))
-	}
+	cmd.Execute()
 }
